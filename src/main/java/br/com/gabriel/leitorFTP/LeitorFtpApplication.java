a249760530c42c4a4549af738d0f255d226d1470
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-19 16:05:21 -0300

    criando projeto

---
+++ src/main/java/br/com/gabriel/leitorFTP/LeitorFtpApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.leitorFTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitorFtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitorFtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/leitorFTP/LeitorFtpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.leitorFTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitorFtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
